d30fe420db3e6e49a0c399b8276653b75a8ea040
Humayra Chowdhury <hc1415926@gmail.com>
2026-03-07 21:53:37 +0000

    Added menu button

--- src/tasks.js
+++ src/tasks.js
@@ -1,7 +1,36 @@
+import { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { Pet, MenuButton, Menu } from './utils.js';
 
 export function Tasks() {
+
+    const[menu, toggleMenu] = useState(false)
+
+    const toggle = () => {
+        toggleMenu(menu => !menu);
+    }
+
+    let theme = "Blue";
+    let themeText = require("./assets/Themes/" + theme + "/Wallpaper.png");
+  
     return (
-        <h1>Tasks Page</h1>
-    )
+        <div className="App" style={{
+            background: 'url(' + themeText + ')',
+            width: '100vw',
+            height: '100vh',
+            backgroundSize: '15rem',
+            imageRendering: 'pixelated',}}>
+
+            <div className = 'container'>
+
+                <div className='top'>
+                    <MenuButton theme={"Blue"} menu={false} toggle={() => toggle()}/>
+                </div>
+
+                {/* CHANGE OPTION TO 'HOME'*/}
+                {menu && <Menu theme={"Blue"} menu={true} toggle={() => toggle()} option1={"Inventory"} option2={"Shop"} option3={"Tasks"}/>}
+
+            </div>      
+        </div>
+    );
 }
