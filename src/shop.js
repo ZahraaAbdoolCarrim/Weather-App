74f870dccf5eb2c162ecd721890470fae22f9b38
Zahraa Abdool Carrim <ZahraaAbdoolCarrim@outlook.com>
2026-03-07 15:15:15 +0000

    Imported from Utils on Shop and Tasks pages

--- src/shop.js
+++ src/shop.js
@@ -1,5 +1,6 @@
 // Muskaan, you can write your code in the shop-functionality.js file
 // Then when the UI is done, we can migrate what we need to this file - Zahraa
+import { Pet, MenuButton, Menu } from './utils.js';
 
 export function Shop() {
     return (
--- src/tasks.js
+++ src/tasks.js
@@ -1,3 +1,5 @@
+import { Pet, MenuButton, Menu } from './utils.js';
+
 export function Tasks() {
     return (
         <h1>Tasks Page</h1>
